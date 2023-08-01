757e5cd6d2ad3185d0cda6f9bc6200f06ceb4efd
Khurshiddd <marifovxurshid80@gmail.com>
2023-08-01 08:56:30 +0500

    first commit

---
+++ resources/views/US/includes/sidbar.blade.php
@@ -0,0 +1,22 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon far fa-clipboard"></i>
+                    Post
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fa-solid fa-bars"></i>
+                        Category
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
